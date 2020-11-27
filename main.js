41b1bcb5cf9c19cac2e9b5e9ae9a2356b7ac7e35
czetoj <czeto.jozsef.0229@gmail.com>
2020-11-27 18:14:23 +0100

    2.0  flex és split megoldás

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
 const display = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.number');
 const clear = document.querySelector('.clear');
-const operatorButtons = document.querySelectorAll('.firstLine');
+const operatorButtons = document.querySelectorAll('.operator');
 const dot = document.querySelector('.dot');
 const equal = document.querySelector('.equal');
 
-let result1 = 0;
-let result2 = 0;
-let result = 0;
+let result1 = 0; let result2 = 0; let result = 0;
+
 display.textContent = 0;
-let numbers = [];
-let n = 0;
-for (let i = 0; i < 100; i++) {
-    numbers[i] = [];
-}
-let operators = [];
+
+let numbers = []; let operators = [];
 
 Array.from(numberButtons).forEach(item => {
     item.addEventListener('click', () => {
         if (display.textContent.startsWith('0')) display.textContent = '';
         display.textContent += item.textContent;
-        numbers[n].push(parseInt(item.id));
     })
 });
 
@@ -30,22 +24,13 @@ Array.from(operatorButtons).forEach(item => {
             return false;
         }
         display.textContent += item.textContent;
-        operators.push(item.id);
-        n++;
     })
 })
 
 clear.addEventListener('click', () => {
     display.textContent = 0;
-    result = 0;
-    result1 = 0;
-    result2 = 0;
-    numbers = [];
-    for (let i = 0; i < 100; i++) {
-        numbers[i] = [];
-    }
-    n = 0;
-    operators = [];
+    result = 0; result1 = 0; result2 = 0;
+    operators = []; numbers = [];
 })
 
 equal.addEventListener('click', () => {
@@ -54,26 +39,45 @@ equal.addEventListener('click', () => {
 })
 
 function handleResult() {
-    for (let i = 0; i < numbers.length; i++) {
-        handleValidOperators();
-        (i > 0) ? result1 = result : result1 = parseInt(numbers[i].join(''));
-        if (numbers[i + 1]) { result2 = parseInt(numbers[i + 1].join('')) }
-        else break;
-        operator = operators[i];
-        switch (operator) {
-            case "+": result = result1 + result2; break;
-            case "*": result = result1 * result2; break;
-            case "/": result = result1 / result2; break;
-            case "-": result = result1 - result2; break;
-        }
+    getNumbersArray();
+    getOperatorsArray();
+    operations();
+    return result;
+}
 
-    }
+function getNumbersArray() {
+    let numbersString = [];
+    numbersString = display.textContent.split(/\D+/);
+    numbersString.forEach(item => {
+        if (typeof parseInt(item) === 'number') {
+            numbers.push(parseInt(item));
+        }
+    });
+}
+
+function getOperatorsArray() {
+    let operatorsString = [];
+    operatorsString = display.textContent.split(/\d/);
+    operators = operatorsString.filter(item => item !== '');
 }
 
 function handleValidOperators() {
-    numbersValid = numbers.filter(item => item.length);
-    if (operators.length >= numbersValid.length) {
-        result = "ERROR";
-        return false;
-    }
+    operators.forEach(item => {
+        if (item.length >= 2) {
+            result = "ERROR";
+            return false;
+        }
+    })
+}
+
+function operations() {
+    result = numbers[0];
+    numbers.shift();
+    numbers.forEach((item, index) => {
+        handleValidOperators();
+        if (operators[index] === "+") { result += item }
+        else if (operators[index] === '-') { result -= item }
+        else if (operators[index] === '÷') { result /= item }
+        else if (operators[index] === 'x') { result *= item };
+    })
 }
